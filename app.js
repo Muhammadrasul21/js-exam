c3578eae5c54d1f460fbd21b1b4f60a831e5be5f
Muhammadrasul21 <theboy6878@gmail.com>
2024-10-18 16:01:07 +0500

    added media

--- app.js
+++ app.js
@@ -1,18 +1,18 @@
 const categoryImages = {
     electronics: 'https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg',
-    jewelry: 'https://fakestoreapi.com/img/71li-ujtlUL._AC_UL640_QL65_ML3_.jpg', // "jewelery" emas, "jewelry" to'g'ri yozilishi
-    "men's clothing": 'https://fakestoreapi.com/img/71YXzeOuslL._AC_UY879_.jpg',
+    jewelry: 'https://fakestoreapi.com/img/71pWzhdJNwL._AC_UL640_QL65_ML3_.jpg', "men's clothing": 'https://fakestoreapi.com/img/71YXzeOuslL._AC_UY879_.jpg',
     "women's clothing": 'https://fakestoreapi.com/img/51Y5NI-I5jL._AC_UX679_.jpg'
 };
 
 fetch('https://fakestoreapi.com/products/categories')
     .then(res => res.json())
     .then(categories => {
-        const categoryGrid = document.getElementById('categoryGrid');
-
+        localStorage.setItem(`categories`, JSON.stringify(categories))
+        
+        const categoryGrid = document.querySelector('#categoryGrid');
         categories.forEach(category => {
             const categoryCard = document.createElement('div');
-            categoryCard.classList.add('category-card');
+            categoryCard.classList.add('category__card');
 
             const imageSrc = categoryImages[category] || 'https://via.placeholder.com/150';
 
