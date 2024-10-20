7b9697534601e375925fb76a4920c8ef58b2cb3d
Muhammadrasul21 <theboy6878@gmail.com>
2024-10-20 14:58:05 +0500

    edited detail page

--- js/product.js
+++ js/product.js
@@ -24,44 +24,48 @@ async function getData() {
 
 function createContent(product) {
     content.innerHTML = `
-        <div>
-            <img src="${product.image}" class="content__image main__image">
-            <h1>${product.title}</h1>
-            <h2>$${product.price}</h2>
-            <p>${product.description}</p>
-            <button>Buy now</button>
-            <hr>
-            <div class="color">
-                <p>Colours:</p>
+ <div class="content">
+
+    <img src="${product.image}" class="content__image main__image">
+
+    <div class="content__info">
+        <h1>${product.title}</h1>
+        <h2>$${product.price}</h2>
+        <p>${product.description}</p>
+        <button>Buy now</button>
+        <hr>
+        <div class="color">
+            <p>Colours:</p>
+        </div>
+        <div class="size">
+            <p>Size:</p>
+            <button>XS</button>
+            <button>S</button>
+            <button>M</button>
+            <button>L</button>
+            <button>XL</button>
+        </div>
+        <div class="bynow">
+            <div class="bynow__count">
+                <button>-</button>
+                <button>2</button>
+                <button>+</button>
             </div>
-            <div class="size">
-                <p>Size:</p>
-                <button>XS</button>
-                <button>S</button>
-                <button>M</button>
-                <button>L</button>
-                <button>XL</button>
+            <button class="seeMore">Buy now</button>
+            <button><i class="fa-regular fa-heart"></i></button>
+        </div>
+        <div class="free">
+            <div class="free_delivery">
+                <p>Free Delivery <br>
+                    Enter your postal code for Delivery Availability</p>
             </div>
-            <div class="bynow">
-                <div class="bynow__count">
-                    <button>-</button>
-                    <button>2</button>
-                    <button>+</button>
-                </div>
-                <button class="seeMore">Buy now</button>
-                <button><i class="fa-regular fa-heart"></i></button>
+            <div class="free__return">
+                <p>Return Delivery <br> Free 30 Days Delivery Returns. Details</p>
             </div>
-            <div class="free">
-                <div class="free_delivery">
-                    <p>Free Delivery <br>
-                        Enter your postal code for Delivery Availability</p>
-                </div>
-                <div class="free__return">
-                    <p>Return Delivery <br> Free 30 Days Delivery Returns. Details</p>
-                </div>
-            </div>
-        </div>`
-    review.innerHTML = '<p>No reviews available.</p>';
+        </div>
+    </div>
+</div>
+       `
 }
 
 window.onload = getData;
